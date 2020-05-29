32f4394857214e8d3fcf1efc384cadec30047d33
zpdev <himoker@163.com>
2020-05-29 22:20:20 +0800

    init

--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -1,245 +1,164 @@
 <template>
-    <div class="monitor">
-        <el-row :gutter="20">
-            <el-col :span="8">
-                <el-card class="tenant">
-                    <div slot="header" class="card_header">
-                        <div class="tenant-icon">
-                            <i class="el-icon-office-building c_h_icon"></i>
-                        </div>
+  <div class="monitor">
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <MBase/>
+      </el-col>
 
-                        <div class="card_h_des">
-                            <div class="c_h_desc_title">租户</div>
-                            <div class="c_h_desc_content">租户总数</div>
-                        </div>
+      <el-col :span="8">
+        <MBase/>
 
-                        <div class="c_h_num">
-                            0
-                        </div>
-                    </div>
-                    <div class="c_main_c">
-                        <div class="tenant_num">
-                            <div class="c_normal">
-                                <div>2</div>
-                                <div class="main_desc">正常</div>
-                            </div>
-                            <div class="c_abnormal">
-                                <div>0</div>
-                                <div class="main_desc">异常</div>
-                            </div>
-                        </div>
-                        <div class="c_main_process">
-                            <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
-                        </div>
-                    </div>
-                </el-card>
-            </el-col>
+      </el-col>
 
-            <el-col :span="8">
-                <el-card class="tenant">
-                    <div slot="header" class="card_header">
-                        <div class="tenant-icon">
-                            <i class="el-icon-office-building c_h_icon"></i>
-                        </div>
+      <el-col :span="8">
+        <MResource/>
+      </el-col>
 
-                        <div class="card_h_des">
-                            <div class="c_h_desc_title">安全服务</div>
-                            <div class="c_h_desc_content">安全服务总数</div>
-                        </div>
+    </el-row>
 
-                        <div class="c_h_num">
-                            0
-                        </div>
-                    </div>
-                    <div class="c_main_c">
-                        <div class="tenant_num">
-                            <div class="c_normal">
-                                <div>2</div>
-                                <div class="main_desc">正常</div>
-                            </div>
-                            <div class="c_abnormal">
-                                <div>0</div>
-                                <div class="main_desc">异常</div>
-                            </div>
-                        </div>
-                        <div class="c_main_process">
-                            <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
-                        </div>
-                    </div>
-                </el-card>
-            </el-col>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card class="license">
+          <div slot="header" class="licnese_header">
+            <span>许可证</span>
+          </div>
+          <div class="license_total">
+            <div class="license_c2">
+              <div class="license_desc_l">
+                <div class="license_desc_lt">
+                  <div class="license_title">等保二</div>
+                  <div class="license_desc">许可证总数</div>
+                </div>
+                <div class="license_desc_num">0</div>
+              </div>
 
-            <el-col :span="8">
-                <el-card class="tenant">
-                    <div slot="header" class="card_header">
-                        <div class="tenant-icon">
-                            <i class="el-icon-office-building c_h_icon"></i>
-                        </div>
+              <div class="license_percent">
+                <div class="license_percent_bar">
+                  <el-progress type="dashboard" :percentage="50"></el-progress>
+                </div>
 
-                        <div class="card_h_des">
-                            <div class="c_h_desc_title">资源</div>
-                            <div class="c_h_desc_content">资源总数</div>
-                        </div>
-
-                        <div class="c_h_num">
-                            0
-                        </div>
-                    </div>
-                    <div class="c_main_c">
-                        <div class="tenant_num">
-                            <div class="c_normal">
-                                <div>2</div>
-                                <div class="main_desc">正常</div>
-                            </div>
-                            <div class="c_abnormal">
-                                <div>0</div>
-                                <div class="main_desc">异常</div>
-                            </div>
-                        </div>
-                        <div class="c_main_process">
-                            <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
-                        </div>
-                    </div>
-                </el-card>
-            </el-col>
-
-        </el-row>
-
-        <el-row :gutter="20">
-            <el-col :span="12">
-                <el-card class="license">
-                    <div slot="header" class="licnese_header">
-                        <span>许可证</span>
-                    </div>
-                    <div class="license_total">
-                        <div class="license_c2">
-                            <div class="license_desc_l">
-                                <div class="license_desc_lt">
-                                    <div class="license_title">等保二</div>
-                                    <div class="license_desc">许可证总数</div>
-                                </div>
-                                <div class="license_desc_num">0</div>
-                            </div>
-
-                            <div class="license_percent">
-                                <div class="license_percent_bar">
-                                    <el-progress type="dashboard" :percentage="50"></el-progress>
-                                </div>
-
-                                <div class="license_percent_desc">
-                                    <div>1</div>
-                                    <div>已用</div>
-                                    <div>1</div>
-                                    <div>剩余</div>
-                                </div>
-                            </div>
+                <div class="license_percent_desc">
+                  <div>1</div>
+                  <div>已用</div>
+                  <div>1</div>
+                  <div>剩余</div>
+                </div>
+              </div>
 
 
-                        </div>
-                        <div class="license_split_border"></div>
-                        <div class="license_c3">
-                            <div class="license_desc_l">
-                                <div class="license_desc_lt">
-                                    <div class="license_title">等保三</div>
-                                    <div class="license_desc">许可证总数</div>
-                                </div>
-                                <div class="license_desc_num">0</div>
-                            </div>
+            </div>
+            <div class="license_split_border"></div>
+            <div class="license_c3">
+              <div class="license_desc_l">
+                <div class="license_desc_lt">
+                  <div class="license_title">等保三</div>
+                  <div class="license_desc">许可证总数</div>
+                </div>
+                <div class="license_desc_num">0</div>
+              </div>
 
 
-                            <div class="license_percent">
-                                <div class="license_percent_bar">
-                                    <el-progress type="dashboard" :percentage="50"></el-progress>
-                                </div>
+              <div class="license_percent">
+                <div class="license_percent_bar">
+                  <el-progress type="dashboard" :percentage="50"></el-progress>
+                </div>
 
-                                <div class="license_percent_desc">
-                                    <div>1</div>
-                                    <div>已用</div>
-                                    <div>1</div>
-                                    <div>剩余</div>
-                                </div>
-                            </div>
+                <div class="license_percent_desc">
+                  <div>1</div>
+                  <div>已用</div>
+                  <div>1</div>
+                  <div>剩余</div>
+                </div>
+              </div>
 
-                        </div>
+            </div>
 
-                    </div>
+          </div>
 
-                    <div class="license_detail">
-                        <el-table
-                                :data="tableData"
-                                style="width: 100%"
-                                :header-cell-style="{background:'rgb(243, 243, 244)',color:'#606266'}">
-                            <el-table-column
-                                    prop="vnf_type"
-                                    label="网元类型"
-                                    width="180">
-                            </el-table-column>
-                            <el-table-column
-                                    prop="license_num"
-                                    label="许可证总数"
-                                    width="180">
-                            </el-table-column>
-                            <el-table-column
-                                    prop="used"
-                                    label="已用">
-                            </el-table-column>
-                        </el-table>
-                    </div>
-                </el-card>
-            </el-col>
-            <el-col :span="12">
-                <el-card class="tenant">
-                    <div slot="header" class="card_header">
-                        <div class="tenant-icon">
-                            <i class="el-icon-office-building c_h_icon"></i>
-                        </div>
+          <div class="license_detail">
+            <el-table
+                :data="tableData"
+                style="width: 100%"
+                :header-cell-style="{background:'rgb(243, 243, 244)',color:'#606266'}">
+              <el-table-column
+                  prop="vnf_type"
+                  label="网元类型"
+                  width="180">
+              </el-table-column>
+              <el-table-column
+                  prop="license_num"
+                  label="许可证总数"
+                  width="180">
+              </el-table-column>
+              <el-table-column
+                  prop="used"
+                  label="已用">
+              </el-table-column>
+            </el-table>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card class="tenant">
+          <div slot="header" class="card_header">
+            <div class="tenant-icon">
+              <i class="el-icon-office-building c_h_icon"></i>
+            </div>
 
-                        <div class="card_h_des">
-                            <div class="c_h_desc_title">网元</div>
-                            <div class="c_h_desc_content">网元总数</div>
-                        </div>
+            <div class="card_h_des">
+              <div class="c_h_desc_title">网元</div>
+              <div class="c_h_desc_content">网元总数</div>
+            </div>
 
-                        <div class="c_h_num">
-                            0
-                        </div>
-                    </div>
-                    <div class="c_main_c">
+            <div class="c_h_num">
+              0
+            </div>
+          </div>
+          <div class="c_main_c">
 
-                        <div id="pieReport" style="width: 100%;height: 300px;"></div>
-                    </div>
-                </el-card>
+            <div id="pieReport" style="width: 100%;height: 300px;"></div>
+          </div>
+        </el-card>
 
-                <el-card class="box-card">
-                    <div slot="header" class="licnese_header">
-                        <span>系统状态</span>
-                    </div>
-                    <div class="system_info">
-                        <div>内存利用率</div>
-                        <div class="c_main_process">
-                            <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
-                        </div>
-                        <div>CPU利用率</div>
-                        <div class="c_main_process">
-                            <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
-                        </div>
-                        <div>磁盘利用率</div>
-                        <div class="c_main_process">
-                            <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
-                        </div>
-                    </div>
-                </el-card>
-            </el-col>
-        </el-row>
+        <el-card class="box-card">
+          <div slot="header" class="licnese_header">
+            <span>系统状态</span>
+          </div>
+          <div class="system_info">
+            <div>内存利用率</div>
+            <div class="c_main_process">
+              <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
+            </div>
+            <div>CPU利用率</div>
+            <div class="c_main_process">
+              <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
+            </div>
+            <div>磁盘利用率</div>
+            <div class="c_main_process">
+              <el-progress :percentage="50" :show-text="false" :stroke-width="8"></el-progress>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
 
-    </div>
+  </div>
 
 </template>
 
 <script>
   import echarts from 'echarts'
+  // import MBase from '@/components/monitor/abnormal/MBase.vue'
+  import MBase from "./monitor/abnormal/MBase";
+  import MResource from "./monitor/abnormal/MResource";
 
   export default {
     name: 'Monitor',
+    components: {MResource, MBase},
+    comments: {
+      MBase,
+      MResource
+    },
     data() {
       return {
         tableData: [{
@@ -362,158 +281,158 @@
 </script>
 
 <style scoped>
-    .tenant-icon {
-        height: 45px;
-        width: 45px;
-        color: white;
-        background: rgb(22, 114, 221);
-        border-radius: 5px;
-    }
+  .tenant-icon {
+    height: 45px;
+    width: 45px;
+    color: white;
+    background: rgb(22, 114, 221);
+    border-radius: 5px;
+  }
 
-    .card_header {
-        display: flex;
-    }
+  .card_header {
+    display: flex;
+  }
 
-    .c_h_icon {
-        font-size: 25px;
-        /*display: table-cell;*/
-        /*vertical-align: middle;*/
-        padding-top: 10px;
-        /*padding: auto auto;*/
-    }
+  .c_h_icon {
+    font-size: 25px;
+    /*display: table-cell;*/
+    /*vertical-align: middle;*/
+    padding-top: 10px;
+    /*padding: auto auto;*/
+  }
 
-    .card_h_des {
-        margin-left: 10px;
-        height: 45px;
-        text-align: left;
-        flex: 1 1;
+  .card_h_des {
+    margin-left: 10px;
+    height: 45px;
+    text-align: left;
+    flex: 1 1;
 
-    }
+  }
 
-    .card_h_des .c_h_desc_title {
-        font-size: 16px;
-        font-weight: 600;
-    }
+  .card_h_des .c_h_desc_title {
+    font-size: 16px;
+    font-weight: 600;
+  }
 
-    .card_h_des .c_h_desc_content {
-        font-size: 14px;
-        font-weight: 400;
-        color: #8c939d;
-        padding-top: 6px;
-    }
+  .card_h_des .c_h_desc_content {
+    font-size: 14px;
+    font-weight: 400;
+    color: #8c939d;
+    padding-top: 6px;
+  }
 
-    .c_h_num {
-        color: green;
-        font-size: 30px;
-        line-height: 45px;
-    }
+  .c_h_num {
+    color: green;
+    font-size: 30px;
+    line-height: 45px;
+  }
 
-    .license_desc_num {
-        color: green;
-        font-size: 25px;
-        line-height: 45px;
-        padding-right: 10px;
-    }
+  .license_desc_num {
+    color: green;
+    font-size: 25px;
+    line-height: 45px;
+    padding-right: 10px;
+  }
 
-    .tenant_num {
-        display: flex;
-    }
+  .tenant_num {
+    display: flex;
+  }
 
-    .licnese_header {
-        text-align: left;
-        font-weight: 600;
-    }
+  .licnese_header {
+    text-align: left;
+    font-weight: 600;
+  }
 
-    .c_normal {
-        flex: 1 1;
-        text-align: left;
-    }
+  .c_normal {
+    flex: 1 1;
+    text-align: left;
+  }
 
-    .c_abnormal {
-        text-align: right;
-    }
+  .c_abnormal {
+    text-align: right;
+  }
 
-    .c_main_process {
-        padding-top: 10px;
-    }
+  .c_main_process {
+    padding-top: 10px;
+  }
 
-    .license_desc {
-        font-size: 14px;
-        font-weight: 400;
-        color: #8c939d;
-    }
+  .license_desc {
+    font-size: 14px;
+    font-weight: 400;
+    color: #8c939d;
+  }
 
-    .main_desc {
-        font-size: 14px;
-        font-weight: 400;
-        padding-top: 10px;
-        color: #8c939d;
-    }
+  .main_desc {
+    font-size: 14px;
+    font-weight: 400;
+    padding-top: 10px;
+    color: #8c939d;
+  }
 
-    .el-card {
-        margin-bottom: 20px;
-    }
+  .el-card {
+    margin-bottom: 20px;
+  }
 
-    .license {
-        height: 100%;
-    }
+  .license {
+    height: 100%;
+  }
 
-    .license_title {
-        font-size: 14px;
-        font-weight: 600;
-    }
+  .license_title {
+    font-size: 14px;
+    font-weight: 600;
+  }
 
-    .license_total {
-        display: flex;
-    }
+  .license_total {
+    display: flex;
+  }
 
-    .license_c2 {
-        flex: 1 1;
-        text-align: left;
-    }
+  .license_c2 {
+    flex: 1 1;
+    text-align: left;
+  }
 
-    .license_c3 {
-        flex: 1 1;
-        text-align: left;
+  .license_c3 {
+    flex: 1 1;
+    text-align: left;
 
-    }
+  }
 
-    .license_desc_l {
-        display: flex;
-    }
+  .license_desc_l {
+    display: flex;
+  }
 
-    .license_desc_lt {
-        flex: 1 1;
-        height: 45px;
-    }
+  .license_desc_lt {
+    flex: 1 1;
+    height: 45px;
+  }
 
-    .license_split_border {
-        height: 175px;
-        margin: 10px 20px 0;
-        border-left: 1px solid #e8e8e8;
-    }
+  .license_split_border {
+    height: 175px;
+    margin: 10px 20px 0;
+    border-left: 1px solid #e8e8e8;
+  }
 
-    .license_percent {
-        display: flex;
-        padding: 10px;
-    }
+  .license_percent {
+    display: flex;
+    padding: 10px;
+  }
 
-    .license_percent_bar {
-        flex: 1 1;
-    }
+  .license_percent_bar {
+    flex: 1 1;
+  }
 
-    .license_percent_desc {
-        padding-left: 20px;
-        padding-top: 20px;
-    }
+  .license_percent_desc {
+    padding-left: 20px;
+    padding-top: 20px;
+  }
 
-    .system_info {
-        text-align: left;
+  .system_info {
+    text-align: left;
 
-    }
+  }
 
-    .license_detail {
-        padding-top: 20px;
-        padding-bottom: 10px;
-    }
+  .license_detail {
+    padding-top: 20px;
+    padding-bottom: 10px;
+  }
 </style>
---
+++ src/components/monitor/abnormal/MSecurityService.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "MSecurityService"
+  }
+</script>
+
+<style scoped>
+
+</style>
